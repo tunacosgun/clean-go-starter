03d22095b59580a5855a4f58cf7785fe68786abe
Tunahan <104358580+tunacosgun@users.noreply.github.com>
2026-04-19 00:47:42 +0300

    Initial commit - Tunasoft Yazılım

---
+++ config/config.go
@@ -0,0 +1,91 @@
+package config
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/caarlos0/env/v11"
+)
+
+type (
+	// Config -.
+	Config struct {
+		App     App
+		HTTP    HTTP
+		Log     Log
+		PG      PG
+		GRPC    GRPC
+		RMQ     RMQ
+		NATS    NATS
+		JWT     JWT
+		Metrics Metrics
+		Swagger Swagger
+	}
+
+	// App -.
+	App struct {
+		Name    string `env:"APP_NAME,required"`
+		Version string `env:"APP_VERSION,required"`
+	}
+
+	// HTTP -.
+	HTTP struct {
+		Port           string `env:"HTTP_PORT,required"`
+		UsePreforkMode bool   `env:"HTTP_USE_PREFORK_MODE" envDefault:"false"`
+	}
+
+	// Log -.
+	Log struct {
+		Level string `env:"LOG_LEVEL,required"`
+	}
+
+	// PG -.
+	PG struct {
+		PoolMax int    `env:"PG_POOL_MAX,required"`
+		URL     string `env:"PG_URL,required"`
+	}
+
+	// GRPC -.
+	GRPC struct {
+		Port string `env:"GRPC_PORT,required"`
+	}
+
+	// RMQ -.
+	RMQ struct {
+		ServerExchange string `env:"RMQ_RPC_SERVER,required"`
+		ClientExchange string `env:"RMQ_RPC_CLIENT,required"`
+		URL            string `env:"RMQ_URL,required"`
+	}
+
+	// NATS -.
+	NATS struct {
+		ServerExchange string `env:"NATS_RPC_SERVER,required"`
+		URL            string `env:"NATS_URL,required"`
+	}
+
+	// JWT -.
+	JWT struct {
+		Secret      string        `env:"JWT_SECRET,required"`
+		TokenExpiry time.Duration `env:"JWT_TOKEN_EXPIRY" envDefault:"24h"`
+	}
+
+	// Metrics -.
+	Metrics struct {
+		Enabled bool `env:"METRICS_ENABLED" envDefault:"true"`
+	}
+
+	// Swagger -.
+	Swagger struct {
+		Enabled bool `env:"SWAGGER_ENABLED" envDefault:"false"`
+	}
+)
+
+// NewConfig returns app config.
+func NewConfig() (*Config, error) {
+	cfg := &Config{}
+	if err := env.Parse(cfg); err != nil {
+		return nil, fmt.Errorf("config error: %w", err)
+	}
+
+	return cfg, nil
+}
